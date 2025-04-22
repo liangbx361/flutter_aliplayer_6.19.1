29c07555e76a6e5c6b2cb6dfb60d30bee0175d55
liangbx <liangbx361@163.com>
2025-04-22 10:47:49 +0800

    style: 删除过时方法

--- android/src/main/java/com/alibaba/fplayer/flutter_aliplayer/FlutterAliplayerPlugin.java
+++ android/src/main/java/com/alibaba/fplayer/flutter_aliplayer/FlutterAliplayerPlugin.java
@@ -25,7 +25,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.StandardMessageCodec;
 import io.flutter.plugin.platform.PlatformView;
 import io.flutter.plugin.platform.PlatformViewFactory;
@@ -72,18 +71,6 @@ public class FlutterAliplayerPlugin extends PlatformViewFactory implements Flutt
         mEventChannel.setStreamHandler(this);
 
         flutterAliFloatWindowManager = new FlutterAliFloatWindowManager(flutterPluginBinding.getApplicationContext());
-    }
-
-    //   This static function is optional and equivalent to onAttachedToEngine. It supports the old
-//   pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-//   plugin registration via this function while apps migrate to use the new Android APIs
-//   post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-//
-//   It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-//   them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-//   depending on the user's project. onAttachedToEngine or registerWith must both be defined
-//   in the same class.
-    public static void registerWith(Registrar registrar) {
     }
 
     @Override
